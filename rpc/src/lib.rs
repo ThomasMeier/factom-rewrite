f72f63164c65c74fcd9b5f7ec8f0393fa3d2beef
Thomas Meier <thomas.meier@citrine.it>
2019-09-03 11:17:52 -0500

    Add gitlab-ci.yml

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -3,4 +3,4 @@
 //! This is the most crucial layer for backwards compatibility. Clients running on factomd must be
 //! able to drop in this version without complication.
 //!
-pub fn start_rpc_server(addr: &str, port: u16) {}
+pub fn start_rpc_server(_addr: &str, _port: u16) {}
